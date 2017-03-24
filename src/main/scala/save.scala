4c42a13547d7b0df1935dd2ae20ab92225e310a5
mohoc <maher.mallem@free.fr>
2017-03-24 19:42:57 +0100

    completed history (unfinished)

--- src/main/scala/save.scala
+++ src/main/scala/save.scala
@@ -1,6 +1,9 @@
 import scala.io.Source
 import scala.util.matching.Regex
 
+import java.io.File
+import java.io.FileWriter
+
 class Move(val pieceType:PieceType.PieceType,
            val fromX:Int, val fromY:Int, val toX:Int, val toY:Int,
            val isCatch:Boolean, val isCheck:Boolean,
@@ -17,6 +20,54 @@ class History()
   var black = "?"
   var result = "*"
   var mode = GameMode.Vanilla
+
+  var dim_x = 8
+  var dim_y = 8
+
+  val pieceTypeAbv = scala.collection.mutable.Map[PieceType.PieceType, String]()
+  
+  def castlingIsKingside(move:Move) =
+  {
+    //The coordinates reported in a castling move are the king's ones.
+    //We check if the king's final position is in the same half
+    //of the chessboard as the beginning position.
+    (2 * move.fromX) / dim_x == (2 * move.toX) / dim_x
+  }
+
+  def makePGN(fileName:String) =
+  {
+    
+    val fw = new FileWriter(new File(String.format("%s.png", fileName)))
+    //fw.write(System.lineSeparator()) //new line
+    fw.write(String.format("[Event \"%s\"]\n", event))
+    fw.write(String.format("[Site \"%s\"]\n", site))
+    fw.write(String.format("[Date \"%s\"]\n", date))
+    fw.write(String.format("[Round \"%s\"]\n", round))
+    fw.write(String.format("[White \"%s\"]\n", white))
+    fw.write(String.format("[Black \"%s\"]\n", black))
+    fw.write(String.format("[Result \"%s\"]\n", result))
+    fw.write(String.format("[Mode \"%s\"]\n\n", mode.toString()))
+
+    var nbCharsInLine = 0
+    /*
+    for (move <- moves)
+    {
+      stringOfMove = ""
+      if move.isCastling
+      {
+        if castlingIsKingside(move)
+          stringOfMove = "O-O"
+        else
+          stringOfMove = "O-O-O"
+      }
+      else
+      {
+        if 
+      }
+    }
+    */
+    //fw.close()
+  }
 }
 
 
