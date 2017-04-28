d56e52e873b8031620a05ac192dd1b4c58f70c9d
mohoc <maher.mallem@free.fr>
2017-04-28 19:06:11 +0200

    implemented minimax

--- src/main/scala/local_ai.scala
+++ src/main/scala/local_ai.scala
@@ -17,9 +17,77 @@ An Alpha-Beta AI.
 */
 class AlphaBetaAI extends SynchPlayer
 {
+    val pieceVal = scala.collection.mutable.Map[PieceType.PieceType, Int](
+        PieceType.Pawn -> 1,
+        PieceType.Rook -> 5,
+        PieceType.Knight -> 3,
+        PieceType.Bishop -> 3,
+        PieceType.Queen -> 9,
+        PieceType.King -> 15,
+        PieceType.ArchBishop -> 7,
+        PieceType.Chancellor -> 7
+    )
+    def eval (game:Game) : Int =
+    {
+        var diffPieceVals = 0
+        for (i <- 0 to game.dim_x-1)
+        {
+            for (j <- 0 to game.dim_y-1)
+            {
+                var p = game.pieceAtPosition(i, j)
+                if (p != null)
+                    diffPieceVals += i + j + pieceVal(p.pieceType)
+            }
+        }
+        return diffPieceVals
+    }
+
+    val minEval = 0
+    val maxEval = 1000
+    private var maxProf = 6
+    private var maxNodes = 1000
+    
+    def computeMinimax(game:Game,isMyTurn:bool,prof:Int,nbNodesEvaluated:Int) : Int =
+    {
+        if (prof > maxProf || nbNodesEvaluated > maxNodes)
+            return eval(game)
+        else
+        {
+            var moves = game.possibleMoves.toArray
+            var moveChosenValue = (if isMyTurn then minEval -1 else maxEval + 1)
+            moves.foreach
+            { moveTest =>
+                nbNodesEvaluated += 1
+                moveTestValue = computeMinimax(game, !isMyTurn, prof + 1, nbNodesEvaluated)  // TODO : here, game = (simulate moveTest in game)
+                if ((isMyTurn && moveTestValue > moveChosenValue) || (!isMyTurn && moveTestValue < moveChosenValue))
+                    moveChosenValue = moveTestValue
+                if (nbNodesEvaluated > maxNodes)
+                    break
+            }
+            return moveChosenValue
+        }
+    }
+    
+    def miniMax(game:Game) : (Int,Int,Int,Int) =
+    {
+        var moves = game.possibleMoves.toArray
+        var moveChosen = moves(0)
+        var moveChosenValue = minEval - 1
+        var moveTestValue = 0
+        moves.foreach
+        { moveTest =>
+            moveTestValue = computeMinimax(game,false,1,1)  // TODO : here, game = (simulate moveTest in game)
+            if (moveTestValue > moveChosenValue)
+            {
+                moveChosen = moveTest
+                moveChosenValue = moveTestValue
+            }
+        }
+        return moveChosen
+    }
+
     override def synchPlay (game:Game) : (Int,Int,Int,Int) =
     {
-        // TODO
-        return game.possibleMoves.toArray.last
+        return miniMax(game)
     }
 }
